4c19186027e399f337a7333644d3da8f50fef50c
VKwamboka <verahnyagaka101@gmail.com>
2024-01-29 20:05:24 +0100

    updated reviews

--- screens/Reports.js
+++ screens/Reports.js
@@ -1,39 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { PieChart } from 'react-native-chart-kit';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView, Dimensions } from "react-native";
+import { PieChart } from "react-native-chart-kit";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import ListItem from "../components/ListItem";
+import { theme } from "../theme";
 
+const { width } = Dimensions.get("window");
 
 const ReportScreen = () => {
   const [expenses, setExpenses] = useState([]);
+
+  const labelRadius = 130;
 
   useEffect(() => {
     // Function to retrieve expenses data from local storage
     const fetchExpenses = async () => {
       try {
         // Fetch expenses data from local storage (implement your own logic)
-        const expensesData = await AsyncStorage.getItem('expenses');
+        const expensesData = await AsyncStorage.getItem("expenses");
         if (expensesData) {
           const parsedExpenses = JSON.parse(expensesData);
           setExpenses(parsedExpenses);
         }
       } catch (error) {
-        console.error('Error fetching expenses:', error);
+        console.error("Error fetching expenses:", error);
       }
     };
 
     fetchExpenses();
   }, []);
-
-  // Function to generate random color
-  const getRandomColor = () => {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  };
 
   // Function to prepare data for the doughnut chart
   const prepareChartData = () => {
@@ -42,11 +37,17 @@ const ReportScreen = () => {
     // Aggregate expenses data by category and calculate total amount spent
     expenses.forEach((expense) => {
       const { category, amount } = expense;
-      if (category && category.label) { // Check if category and label are defined
+      if (category && category.label) {
+        // Check if category and label are defined
         if (categoryData[category.label]) {
-          categoryData[category.label] += amount;
+          categoryData[category.label].value += amount;
         } else {
-          categoryData[category.label] = amount;
+          categoryData[category.label] = {
+            value: amount,
+            color: category.color, 
+            legendFontColor: "#7F7F7F",
+            legendFontSize: 15,
+          };
         }
       }
     });
@@ -54,42 +55,90 @@ const ReportScreen = () => {
     // Convert aggregated data to format suitable for pie chart
     const doughnutChartData = Object.keys(categoryData).map((label) => ({
       name: label,
-      value: categoryData[label],
-      color: getRandomColor(), // Generate random color for each category
-      legendFontColor: '#7F7F7F',
-      legendFontSize: 15,
+      value: categoryData[label].value,
+      color: categoryData[label].color,
+      legendFontColor: categoryData[label].legendFontColor,
+      legendFontSize: categoryData[label].legendFontSize,
     }));
+    console.log(doughnutChartData);
+
+    const renderLegends = () => {
+      const angleStep = (2 * Math.PI) / doughnutChartDat.length;
+      return doughnutChartDat.map((legend, index) => {
+        const angle = index * angleStep;
+        const x = labelRadius * Math.cos(angle);
+        const y = labelRadius * Math.sin(angle);
+        return (
+          <View key={index} style={[styles.legendContainer, { left: width / 2 + x, top: width / 2 + y }]}>
+            <Text style={[styles.legend, { color: legend.legendFontColor, fontSize: legend.legendFontSize }]}>
+              {legend.name}
+            </Text>
+          </View>
+        );
+      });
+    };
 
     return doughnutChartData;
   };
 
+ 
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      {expenses.length > 0 ? (
-        <PieChart
-          data={prepareChartData()}
-          width={300}
-          height={220}
-          chartConfig={{
-            backgroundColor: '#ffffff',
-            decimalPlaces: 2,
-            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-            labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-            style: {
-              borderRadius: 16,
-            },
-          }}
-          accessor="value"
-          backgroundColor="transparent"
-          paddingLeft="15"
-          hasLegend={true} // Enable legend to make the chart a doughnut chart
-          center={[10, 10]} // Adjust the center to create a doughnut chart effect
-          absolute // Render values as absolute numbers
-        />
-      ) : (
-        <Text>No expenses data available</Text>
-      )}
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1 }}>
+        {expenses.length > 0 ? (
+          <PieChart
+            data={prepareChartData()}
+            width={330}
+            height={220}
+            chartConfig={{
+              backgroundColor: "#ffffff",
+              decimalPlaces: 2,
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+              labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+              style: {
+                borderRadius: 16,
+              },
+            }}
+            accessor="value"
+            backgroundColor="transparent"
+            paddingLeft="15"
+          
+            hasLegend={true} // Enable legend to make the chart a doughnut chart
+            center={[10, 10]} // Adjust the center to create a doughnut chart effect
+            absolute // Render values as absolute numbers
+          />
+               
+        ) : (
+          <Text>No expenses data available</Text>
+        )}
+
+        <View>
+          <View style={{ borderRadius: 20, overflow: "hidden" }}>
+            {expenses.map((expense) => (
+              <ListItem
+                key={expense._id}
+                detail={
+                  <View
+                    style={{
+                      width: 12,
+                      height: 12,
+                      borderRadius: 6,
+                      marginRight: 10,
+                      backgroundColor: expense.category
+                        ? expense.category.color
+                        : "gray", // Check if category is defined
+                    }}
+                  />
+                }
+                label={expense.category ? expense.category.label : "Unknown"}
+                // labels={expense._id}
+              />
+            ))}
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
