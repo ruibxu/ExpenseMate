a65777d1c54bf2173912fc26dfb9cbe7db67e958
VKwamboka <verahnyagaka101@gmail.com>
2024-02-01 21:12:29 +0100

    updated page styling

--- screens/Reports.js
+++ screens/Reports.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import {
   View,
   Text,
@@ -12,10 +12,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import ListItem from "../components/ListItem";
 import { theme } from "../theme";
 import { Picker } from "@react-native-picker/picker";
+import { AntDesign } from '@expo/vector-icons';
+import { ExpensesContext } from "../context/ExpenseReortContext";
 
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const { width } = Dimensions.get("window");
+
 
 const ReportScreen = () => {
   const [expenses, setExpenses] = useState([]);
@@ -25,6 +27,10 @@ const ReportScreen = () => {
   const years = Array.from({ length: currentYear - 2019 }, (_, i) => 2020 + i);
   const [totalAmountSpent, setTotalAmountSpent] = useState(0);
   const [categorySummary, setCategorySummary] = useState({});
+
+  // const { expenses, setExpenses } = useContext(ExpensesContext);
+
+
 
   // Function to handle month selection
   const handleMonthSelect = (month) => {
@@ -51,6 +57,7 @@ const ReportScreen = () => {
     "OCTOBER",
     "NOVEMBER",
     "DECEMBER",
+    
   ];
 
   useEffect(() => {
@@ -115,7 +122,7 @@ const ReportScreen = () => {
   const calculateCategorySummary = (filteredExpenses) => {
     const categorySummary = {};
     let totalExpense = 0;
-  
+
     // Calculate total expenses and expenses per category
     filteredExpenses?.forEach((expense) => {
       const { category, amount } = expense;
@@ -132,51 +139,48 @@ const ReportScreen = () => {
         }
       }
     });
-  
+
     // Calculate percentage and add it to each category summary
     Object.keys(categorySummary).forEach((category) => {
       categorySummary[category].percentage =
         (categorySummary[category].value / totalExpense) * 100;
     });
-  
+
     return categorySummary;
   };
-  
+
   useEffect(() => {
     // Filter expenses by selected month and year
-    const filteredExpenses = expenses.filter(expense => {
-      const [month, day, year] = expense.date.split('/').map(Number);
+    const filteredExpenses = expenses.filter((expense) => {
+      const [month, day, year] = expense.date.split("/").map(Number);
       return month === selectedMonth && year === selectedYear;
     });
-  
+
     // Calculate category summary for the filtered expenses
     const newCategorySummary = calculateCategorySummary(filteredExpenses);
-  
+
     // Update category summary state
     setCategorySummary(newCategorySummary);
   }, [expenses, selectedMonth, selectedYear]);
-  
 
   const categorySummaryLength = Object.keys(categorySummary).length;
 
-
   useEffect(() => {
     // Filter expenses by selected month and year
-    const filteredExpenses = expenses.filter(expense => {
-      const [month, day, year] = expense.date.split('/').map(Number);
+    const filteredExpenses = expenses.filter((expense) => {
+      const [month, day, year] = expense.date.split("/").map(Number);
       return month === selectedMonth && year === selectedYear;
     });
-  
+
     // Calculate total amount spent for the filtered expenses
     let total = 0;
     filteredExpenses.forEach((expense) => {
       total += expense.amount;
     });
-  
+
     // Update the total amount spent state
     setTotalAmountSpent(total);
   }, [expenses, selectedMonth, selectedYear]);
-  
 
   const [summaryExpanded, setSummaryExpanded] = useState(false);
 
@@ -188,51 +192,49 @@ const ReportScreen = () => {
   return (
     <ScrollView>
       <View style={{ flex: 1 }}>
-        <View style={styles.filterContaine}>
-          <Text style={styles.title}>Month: {months[selectedMonth - 1]}</Text>
-          <Text style={styles.title}>Year: {selectedYear}</Text>
+        <View style={styles.filterContainer}>
+          {/* <View style={styles.datepicker}>
+            <Text style={styles.titleDate}> {months[selectedMonth - 1]}</Text>
+            <Text style={styles.titleDate}>{selectedYear}</Text>
+          </View> */}
 
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Text style={styles.title}>Month:</Text>
-            <Picker
-              selectedValue={selectedMonth}
-              style={{ height: 50, width: 300, color: "white" }}
-              onValueChange={(itemValue) => handleMonthSelect(itemValue)}
-            >
-              {months.map((month, index) => (
-                <Picker.Item label={month} value={index + 1} key={month} />
-              ))}
-            </Picker>
-          </View>
+          <View style={styles.datepicker}>
+            <View style={[styles.titleDate,{ flexDirection: "row", alignItems: "center" }]}>
+              {/* <Text style={styles.title}>Month:</Text> */}
+              
+              <Picker
+                selectedValue={selectedMonth}
+                style={[styles.titleDate,{ height: 50, width: 180 }]}
+                onValueChange={(itemValue) => handleMonthSelect(itemValue)}
+                dropdownIconColor="white"
+              >
+                {months.map((month, index) => (
+                  <Picker.Item  label={month} value={index + 1} key={month} />
+                ))}
+              </Picker>
+              {/* <AntDesign name="caretdown" size={24} color="white" /> */}
+            </View>
 
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Text style={styles.title}>Year:</Text>
-            <Picker
-              selectedValue={selectedYear}
-              style={{ height: 50, width: 200, color: "white" }}
-              onValueChange={(itemValue) => handleYearSelect(itemValue)}
-            >
-              {years.map((year) => (
-                <Picker.Item
-                  label={year.toString()}
-                  value={year}
-                  key={year.toString()}
-                />
-              ))}
-            </Picker>
+            <View style={[styles.titleDate,{ flexDirection: "row", alignItems: "center" }]}>
+              {/* <Text style={styles.title}>Year:</Text> */}
+              <Picker
+                selectedValue={selectedYear}
+                style={[styles.titleDate,{ height: 50, width: 150, color: "white" }]}
+                onValueChange={(itemValue) => handleYearSelect(itemValue)}
+                dropdownIconColor="white"
+              >
+                {years.map((year) => (
+                  <Picker.Item
+                  
+                    label={year.toString()}
+                    value={year}
+                    key={year.toString()}
+                  />
+                ))}
+              </Picker>
+              {/* <AntDesign name="caretdown" size={24} color="white" /> */}
+            </View>
           </View>
-          {/* <Text style={styles.header}>{`${getMonthName(
-            selectedMonth
-          )} ${selectedYear}`}</Text>
-          <TouchableOpacity
-            onPress={() => handleMonthSelect(selectedMonth + 1)}
-          >
-            <MaterialCommunityIcons
-              name="calendar-search"
-              size={34}
-              color="white"
-            />
-          </TouchableOpacity> */}
         </View>
 
         {/* Render the pie chart */}
@@ -276,55 +278,60 @@ const ReportScreen = () => {
         </Text>
         {/* Render the summary view */}
         <TouchableOpacity onPress={toggleSummary}>
-        {categorySummaryLength > 0 ? (
-          <View
-            style={[styles.summary, summaryExpanded && styles.expandedSummary]}
-          >
+          {categorySummaryLength > 0 ? (
             <View
-              style={{
-                flexDirection: "column",
-                alignItems: "center",
-                paddingTop: 15,
-              }}
+              style={[
+                styles.summary,
+                summaryExpanded && styles.expandedSummary,
+              ]}
             >
-              <View style={styles.line}></View>
-              <Text style={styles.header}>SUMMARY</Text>
-            </View>
-
-            <View style={styles.titleArrange}>
-              <Text style={styles.title}>Category</Text>
-              <Text style={styles.title}>Percentage</Text>
-              <Text style={styles.title}>Amount</Text>
-            </View>
-            <ScrollView>
-              <View>
-                {Object.keys(categorySummary).map((category) => (
-                  <ListItem
-                    key={category}
-                    detail={<Text></Text>}
-                    label={
-                      <Text
-                        style={{
-                          color: categorySummary[category].color,
-                          width: 120,
-                        }}
-                      >
-                        {category}
-                      </Text>
-                    }
-                    // Calculate percentage and value for each category
-                    subtitle={`$ ${categorySummary[category].value.toFixed(2)}`}
-                    percentage={`${categorySummary[category].percentage.toFixed(
-                      2
-                    )}% `}
-                  />
-                ))}
+              <View
+                style={{
+                  flexDirection: "column",
+                  alignItems: "center",
+                  paddingTop: 15,
+                }}
+              >
+                <View style={styles.line}></View>
+                <Text style={styles.header}>SUMMARY</Text>
               </View>
-            </ScrollView>
-          </View>
-        ):(
-          <Text style={styles.header}>No expense data available</Text>
-        )}
+
+              <View style={styles.titleArrange}>
+                <Text style={styles.title}>Category</Text>
+                <Text style={styles.title}>Percentage</Text>
+                <Text style={styles.title}>Amount</Text>
+              </View>
+              <ScrollView>
+                <View>
+                  {Object.keys(categorySummary).map((category) => (
+                    <ListItem
+                      key={category}
+                      detail={<Text></Text>}
+                      label={
+                        <Text
+                          style={{
+                            color: categorySummary[category].color,
+                            width: 120,
+                          }}
+                        >
+                          {category}
+                        </Text>
+                      }
+                      // Calculate percentage and value for each category
+                      subtitle={`$ ${categorySummary[category].value.toFixed(
+                        2
+                      )}`}
+                      percentage={`${categorySummary[
+                        category
+                      ].percentage.toFixed(2)}% `}
+                    />
+                  ))}
+                </View>
+              </ScrollView>
+            </View>
+          ) : (
+            <Text style={styles.header}>No expense data available</Text>
+          )}
         </TouchableOpacity>
       </View>
     </ScrollView>
@@ -340,10 +347,33 @@ const styles = StyleSheet.create({
     fontSize: 24,
     textAlign: "center",
     padding: 14,
+  
   },
   title: {
     color: "white",
     fontSize: 18,
+  },
+  titleDate:{
+    color: "white",
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "white",
+    borderTopWidth: 1,
+    borderTopColor: "white",
+    borderLeftWidth: 1,
+    borderLeftColor: "white",
+    borderRightWidth: 1,
+    borderRightColor: "white",
+    // paddingLeft:10,
+    // paddingRight:10,
+    paddingTop:2,
+    paddingBottom:2,
+    backgroundColor:theme.colors.card,
+    borderRadius:10,
+    // width: 150,
+    textAlign:'center'
+
+
   },
   titleArrange: {
     flexDirection: "row",
@@ -376,11 +406,17 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   filterContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginBottom: 10,
+    // flexDirection: "row",
+    // justifyContent: "space-between",
+    // alignItems: "center",
+    // marginBottom: 10,
   },
+  datepicker:{
+     flexDirection: "row",
+    justifyContent: "space-between",
+    
+    padding: 10,
+  }
 });
 
 export default ReportScreen;
